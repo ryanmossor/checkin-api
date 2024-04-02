e5cfdfde079edde41b6775e0077b85a8b5c25e9e
Ryan Mossor <ryan.mossor@gmail.com>
2024-04-01 20:44:49 -0500

    removes unprocessed field

--- src/Models/CheckinResponse.cs
+++ src/Models/CheckinResponse.cs
@@ -5,14 +5,12 @@ namespace CheckinApi.Models;
 public class CheckinResponse
 {
     public List<CheckinResult> Results { get; set; }
-    public int ProcessedCount { get; set; }
     public List<CheckinItem> Unprocessed { get; set; }
 
     [JsonConstructor]
     public CheckinResponse(List<CheckinResult> results, List<CheckinItem>? unprocessed = null)
     {
         Results = results;
-        ProcessedCount = results.Count;
         Unprocessed = unprocessed ?? new List<CheckinItem>();
     }
 }
